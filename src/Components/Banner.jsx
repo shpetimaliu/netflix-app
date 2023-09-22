911cc706857b6845e786bd121c95a9b878ab1005
Shpetim <shpetimmaliu@gmail.com>
2023-09-22 22:23:07 +0200

    integrate api TMDB

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,38 +1,48 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Requests from "../api/Requests";
 import "./Banner.css";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetch() {
+      const request = await axios.get(Requests.fetchNetflixOriginals);
+      setMovie(
+        request?.data?.results[
+          Math.floor(Math.random() * request?.data?.results.length - 1)
+        ]
+      );
+    }
+    fetch();
+  }, []);
+
+  console.log(movie);
+
   function shortText(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
+
   return (
     <div
       className="Banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url('https://images.pexels.com/videos/3045163/free-video-3045163.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500')`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
         <h1 className="banner__description">
-          {shortText(
-            `Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.`,
-            150
-          )}
+          {shortText(movie?.overview, 150)}
         </h1>
       </div>
       <div className="banner--fadeBottom"></div>
---
+++ src/api/Requests.js
@@ -0,0 +1,15 @@
+// const API_KEY = process.env.REACT_APP_TMDB;
+const API_KEY = "4c89b442b1e661bce6d7336ffda23fca";
+
+const Requests = {
+  fetchTrading: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentariesMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default Requests;
