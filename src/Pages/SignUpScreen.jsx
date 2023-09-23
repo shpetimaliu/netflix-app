9c0ab07b750d193d7bc1cf428c48cb0acf5729cc
Shpetim <shpetimmaliu@gmail.com>
2023-09-23 15:25:50 +0200

    Update css for Signup/Login Screen

--- src/Pages/SignUpScreen.jsx
+++ src/Pages/SignUpScreen.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./SignUpScreen.css";
 
 function SignUpScreen() {
-  return <div>SignUpScreen</div>;
+  return (
+    <div className="SignUpScreen">
+      <form>
+        <h1>Sign In / Sign Up</h1>
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+        <button>Sign In</button>
+        <h3>
+          New to Netflix? <a href="/signup">Signup Now</a>
+        </h3>
+        <p>
+          This page is protected by Google reCAPTCHA to ensure you're not a bot.
+          Learn more.
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default SignUpScreen;
