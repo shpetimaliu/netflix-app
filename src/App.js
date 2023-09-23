c46503ed150822d830efe6da66406006c47701cb
Shpetim <shpetimmaliu@gmail.com>
2023-09-23 02:13:09 +0200

    Start creating user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import HomeScreen from "./Pages/HomeScreen";
 
 function App() {
   return (
     <div className="App">
-      <HomeScreen />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomeScreen />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
