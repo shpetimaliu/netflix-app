a8b3f72b03b30b17dacc7d4eab6e910f9a8c8898
Shpetim <shpetimmaliu@gmail.com>
2023-09-23 15:00:50 +0200

    Start signup page

--- src/Pages/LoginScreen.jsx
+++ src/Pages/LoginScreen.jsx
@@ -1,29 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginScreen.css";
+import SignUpScreen from "./SignUpScreen";
 
 function LoginScreen() {
+  const [signIn, setSignIn] = useState(false);
+
   return (
     <div className="LoginScreen">
       <div className="login_background">
         <img className="login__logo" src="netflix.svg" alt="logo" />
-        <button className="loginScreen__button">Sign In</button>
+        <button onClick={() => setSignIn(true)} className="loginScreen__button">
+          Sign In
+        </button>
         <div className="loginScreen__fade" />
       </div>
       <div className="body">
-        <>
-          <h1>Unlimited movies, TV shows, and more</h1>
-          <h2>Watch anywhere. Cancel anytime.</h2>
-          <p>
-            Ready to watch? Enter your email to create or restart your
-            membership.
-          </p>
-          <div className="input">
-            <form>
-              <input type="email" placeholder="Email address" />
-              <button className="button__getStarted">Get Started &gt;</button>
-            </form>
-          </div>
-        </>
+        {signIn ? (
+          <SignUpScreen />
+        ) : (
+          <>
+            <h1>Unlimited movies, TV shows, and more</h1>
+            <h2>Watch anywhere. Cancel anytime.</h2>
+            <p>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </p>
+            <div className="input">
+              <form>
+                <input type="email" placeholder="Email address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="button__getStarted"
+                >
+                  Get Started &gt;
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
