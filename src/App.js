af9278823ea155e2ee575d36d9f0d33e0d3284eb
Shpetim <shpetimmaliu@gmail.com>
2023-09-23 16:25:09 +0200

    I use counterRedux for select is user Login/Logout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import HomeScreen from "./Pages/HomeScreen";
 import LoginScreen from "./Pages/LoginScreen";
+import { login, logout, selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscriber = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+        console.log("userAuth:", userAuth);
+      } else {
+        dispatch(logout);
+      }
+    });
+
+    return unsubscriber;
+  }, []);
 
   return (
     <div className="App">
