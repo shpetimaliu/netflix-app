8485d72a6c05d2075b8eb8248477423b2da931f5
Shpetim <shpetimmaliu@gmail.com>
2023-09-23 02:44:05 +0200

    update LoginScreen Page

--- src/Pages/LoginScreen.jsx
+++ src/Pages/LoginScreen.jsx
@@ -7,6 +7,23 @@ function LoginScreen() {
       <div className="login_background">
         <img className="login__logo" src="netflix.svg" alt="logo" />
         <button className="loginScreen__button">Sign In</button>
+        <div className="loginScreen__fade" />
+      </div>
+      <div className="body">
+        <>
+          <h1>Unlimited movies, TV shows, and more</h1>
+          <h2>Watch anywhere. Cancel anytime.</h2>
+          <p>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </p>
+          <div className="input">
+            <form>
+              <input type="email" placeholder="Email address" />
+              <button className="button__getStarted">Get Started &gt;</button>
+            </form>
+          </div>
+        </>
       </div>
     </div>
   );
