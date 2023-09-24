89f17fc5ff6d90a314ceb5dbd8bee08d79a17a17
Shpetim <shpetimmaliu@gmail.com>
2023-09-24 18:24:09 +0200

    done ✅

--- src/Components/Plans.jsx
+++ src/Components/Plans.jsx
@@ -2,13 +2,37 @@ import React from "react";
 import "./Plans.css";
 
 function Plans() {
+  let products = [
+    {
+      name: "Basic Plan",
+      descrition: "1080p + HDR + Ads Free",
+      subscribed: false,
+    },
+    {
+      name: "Standard Plan",
+      descrition: "4k + HDR + Ads Free",
+      subscribed: false,
+    },
+    {
+      name: "Premium Plan",
+      descrition: "4k + Serial + HDR + Ads Free",
+      subscribed: true,
+    },
+  ];
+
   return (
-    <div className="planScreen__plan">
-      <div className="planScreen__info">
-        <h5>Premium Plan</h5>
-        <h6>4K + HDR + Ads Free</h6>
-      </div>
-      <button>Subscribe</button>
+    <div className="plansScreen">
+      {products.map((product, index) => (
+        <div key={index} className="planScreen__plan">
+          <div className="planScreen__info">
+            <h5>{product.name}</h5>
+            <h6>{product.descrition}</h6>
+          </div>
+          <button className={product.subscribed ? "subscribed" : ""}>
+            {product.subscribed ? "Subscribed" : "Subscribe"}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
